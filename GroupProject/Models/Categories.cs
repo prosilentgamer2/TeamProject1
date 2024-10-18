fddb8ecb624e6b7ff27aaac71b87d246daeaae4f
mdmontes94 <156222980+mdmontes94@users.noreply.github.com>
2024-10-17 19:56:12 -0500

    Added code to the categories model

---
+++ GroupProject/Models/Categories.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupProject.Models
+{
+
+    public class Categories
+    {
+        public int CategoryID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+}
